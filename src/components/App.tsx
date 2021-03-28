ad60f39f9660e17dcf69b46158bf2b6b952d952d
Nwagu Victor <leofizzy3@gmail.com>
2021-03-28 01:49:22 +0100

    init: initial app setup

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dashboard from '../pages/Dashboard'
+
+const App: React.FC = (): JSX.Element => {
+    return (
+        <div>
+            <Dashboard />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.module.scss'
+
+const Dashboard: React.FC = (): JSX.Element => {
+    return (
+        <div>
+            DashBoard
+        </div>
+    )
+}
+
+export default Dashboard;
